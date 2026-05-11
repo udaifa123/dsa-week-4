457ea3c4a768e5eda9f8014402f20bdb3d18527f
udaifa123 <udaifakk509@gmail.com>
2026-05-11 14:13:04 +0530

    Create two-sum.js

---
+++ two-sum.js
@@ -0,0 +1,15 @@
+function twoSum(nums, target){
+  let map = {}
+
+  for(let i=0;i<nums.length;i++){
+    let diff = target - nums[i]
+
+    if(map[diff] !== undefined){
+      return [map[diff], i]
+    }
+
+    map[nums[i]] = i
+  }
+}
+
+console.log(twoSum([2,7,11,15], 9))
